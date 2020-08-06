5cfd41e873a705a346305379a5cb1dc0c2b6ec68
Ramarao <ramarao.mutyala@gmail.com>
2020-08-06 13:04:45 +0530

    my commit

---
+++ MyApp/MyApp/src/main/java/com/ofss/TestProduct.java
@@ -0,0 +1,11 @@
+package com.ofss;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		Product p=new Product("SmartPhone",45000);
+		System.out.println(p.getProductName()+" and "+p.getProductPrice());
+		System.out.println("Woow it is done");
+	}
+
+}
